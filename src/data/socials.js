a9332a608680f0180cc1b340bb6fd583c54171f9
lalit2406 <lalitkumar931536@gmail.com>
2026-07-20 13:44:05 +0530

    fixed the resume link in socials.js to point to the correct PDF file

--- src/data/socials.js
+++ src/data/socials.js
@@ -2,5 +2,5 @@ export const socials = {
   email: 'lalitkumar931536@gmail.com',
   github: 'https://github.com/lalit2406',
   linkedin: 'https://www.linkedin.com/in/lalit-kumar-web/',
-  resume: '/resume/Lalit_Kumar_Resume.pdf',
+  resume: '/resume/Lalit_Resume.pdf',
 };
